2ef90278679b1a0536d1e33a29ff366e8f7d8b5a
Justin Chick <hey@justinchick.com>
2013-12-01 16:37:49 -0600

    homepage slider

--- templates/content-carousel.php
+++ templates/content-carousel.php
@@ -1,38 +1,31 @@
-<div id="myCarousel" class="carousel slide">
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+  </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
-        <div class="carousel-caption">
-          <h1>Modern Business - A Bootstrap 3 Template</h1>
-        </div>
-      </div>
-      <div class="item">
-        <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-        <div class="carousel-caption">
-          <h1>Ready to Style &amp; Add Content</h1>
-        </div>
-      </div>
-      <div class="item">
-        <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-        <div class="carousel-caption">
-          <h1>Additional Layout Options at <a href="http://startbootstrap.com">http://startbootstrap.com</a></h1>
-        </div>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="http://placehold.it/1900x1080&text=Slide One">
+      <div class="carousel-caption">
+        <h1>This is just a test</h1>
       </div>
     </div>
+    <div class="item">
+      <img src="http://placehold.it/1900x1080&text=Slide Two">
+      <div class="carousel-caption">
+        <h1>This is just a test</h1>
+      </div>
+    </div>
+  </div>
 
-    <!-- Controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="icon-prev"></span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="icon-next"></span>
-    </a>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
 </div>
