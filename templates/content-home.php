639c8014a67d4ce639c0b1ba648a33b1ad8ac3cf
Justin Chick <hey@justinchick.com>
2013-12-01 17:03:07 -0600

    connect font awesome less

--- templates/content-home.php
+++ templates/content-home.php
@@ -1,17 +1,17 @@
 <div class="section">
 	<div class="row">
 		<div class="col-lg-4 col-md-4">
-			<h3><i class="fa fa-check-circle"></i>Section Title</h3>
+			<h3><i class="fa fa-check-circle"></i> Section Title</h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
 		</div>
 
 		<div class="col-lg-4 col-md-4">
-			<h3><i class="fa fa-check-circle"></i>Section Title</h3>
+			<h3><i class="fa fa-check-circle"></i> Section Title</h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
 		</div>
 
 		<div class="col-lg-4 col-md-4">
-			<h3><i class="fa fa-check-circle"></i>Section Title</h3>
+			<h3><i class="fa fa-check-circle"></i> Section Title</h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
 		</div>
 	</div>
