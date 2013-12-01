79db48d1f78c7638dc5ae9750a6bc29137173c92
Justin Chick <hey@justinchick.com>
2013-12-01 17:14:20 -0600

    credit to home template

--- templates/content-home.php
+++ templates/content-home.php
@@ -1,18 +1,26 @@
 <div class="section">
 	<div class="row">
 		<div class="col-lg-4 col-md-4">
-			<h3><i class="fa fa-check-circle"></i> Section Title</h3>
+			<h3><i class="fa fa-check-circle"></i> <a href="http://wordpress.org">Wordpress</a> Powered</h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
 		</div>
 
 		<div class="col-lg-4 col-md-4">
-			<h3><i class="fa fa-check-circle"></i> Section Title</h3>
+			<h3><i class="fa fa-pencil"></i> Built on <a href="http://roots.io">Roots</a></h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
 		</div>
 
 		<div class="col-lg-4 col-md-4">
-			<h3><i class="fa fa-check-circle"></i> Section Title</h3>
+			<h3><i class="fa fa-folder-open"></i> Design by <a href="http://startbootstrap.com">Start Bootstrap</a></h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
 		</div>
+	</div>
+</div>
+
+<div class="section-colored text-center">
+	<div class="col-lg-12">
+		<h2>Modern Business: A Clean &amp; Simple Full Wordpress Template by <a href="http://justinchick.com">Jaechick</a></h2>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+		<hr />
 	</div>
 </div>
