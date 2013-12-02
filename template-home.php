2c80ea19d5baff2da60231295d614740797b0403
Justin Chick <hey@justinchick.com>
2013-12-02 14:42:41 -0600

    home button template

--- template-home.php
+++ template-home.php
@@ -8,3 +8,4 @@ Template Name: Home Template
 <?php get_template_part('templates/content', 'home'); ?>
 <?php get_template_part('templates/content', 'homeSection' ); ?>
 <?php get_template_part('templates/content', 'homeSectionColored' ); ?>
+<?php get_template_part('templates/content', 'homeButton' ); ?>
