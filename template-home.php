87ef109f643ddf2c42245974f2b39dcb9abb7fa4
Justin Chick <hey@justinchick.com>
2013-12-02 14:56:09 -0600

    rename home carousel template

--- template-home.php
+++ template-home.php
@@ -4,7 +4,7 @@ Template Name: Home Template
 */
 ?>
 
-<?php get_template_part('templates/content', 'carousel'); ?>
+<?php get_template_part('templates/content', 'homeCarousel'); ?>
 <?php get_template_part('templates/content', 'home'); ?>
 <?php get_template_part('templates/content', 'homeSection' ); ?>
 <?php get_template_part('templates/content', 'homeSectionColored' ); ?>
