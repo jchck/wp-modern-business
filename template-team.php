7c61601e6b8963455bf06bc720783f17dd0991ae
Justin Chick <hey@justinchick.com>
2013-12-02 16:20:42 -0600

    finalize team page

--- template-team.php
+++ template-team.php
@@ -6,3 +6,10 @@ Template Name: Team Template
 
 <?php get_template_part('templates/page', 'header'); ?>
 
+<div class="row">
+	<div class="col-lg-12">
+		<?php get_template_part('templates/content', 'page'); ?>
+	</div>
+</div>
+
+<?php get_template_part('templates/page', 'teamBios'); ?>
