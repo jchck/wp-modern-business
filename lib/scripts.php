990d4814662a5040f654bf587e3089b223c4578c
Justin Chick <hey@justinchick.com>
2013-12-02 16:04:12 -0600

    margins to align left/right img classes

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '87001c169bf4716fcf2653a9d5997616');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'faea4cb3d6e8b1fde802f4bae9480179');
 
   //wp_enqueue_script('roots_main', get_template_directory_uri() . '/assets/css/app.css', false, false);
 
