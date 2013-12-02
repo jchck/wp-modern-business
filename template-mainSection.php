84906e636b51aeb8ec9e3628207af113a4a5e1af
Justin Chick <hey@justinchick.com>
2013-12-02 15:43:30 -0600

    main section page template finalize

--- template-mainSection.php
+++ template-mainSection.php
@@ -5,3 +5,14 @@ Template Name: Main Section Page
 ?>
 
 <?php get_template_part('templates/page', 'header'); ?>
+
+<div class="row">
+	<div class="col-lg-12">
+		<img class="img-responsive" src="http://placehold.it/1200x300" />
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-12">
+		<?php get_template_part('templates/content', 'page'); ?>
+	</div>
+</div>
