8d7b89d7dcb6d2c34ea5d2136afde1814336502b
Justin Chick <hey@justinchick.com>
2013-12-02 19:30:34 -0600

    grammar on 404 page

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
+  <?php _e('Sorry, but the page you are trying to view does not exist.', 'roots'); ?>
 </div>
 
 <p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
